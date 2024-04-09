241ca05fd60705384a59cc6180a08b9a4e32ab5f
Son Tran Lam <sontl@mac.com>
2024-04-09 14:33:21 +0800

    update brand in homepage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ChatbotUISVG } from "@/components/icons/chatbotui-svg"
+import { Brand } from "@/components/ui/brand"
 import { IconArrowRight } from "@tabler/icons-react"
 import { useTheme } from "next-themes"
 import Link from "next/link"
@@ -10,11 +10,7 @@ export default function HomePage() {
 
   return (
     <div className="flex size-full flex-col items-center justify-center">
-      <div>
-        <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
-      </div>
-
-      <div className="mt-2 text-4xl font-bold">Kế Toán AI</div>
+      <Brand />
 
       <Link
         className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
